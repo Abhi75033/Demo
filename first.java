b5801b37b5e671aa2997802fba48fb336513df4c
Abhi75033 <abhishekyadav1112.21@gmail.com>
2023-08-20 19:24:20 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,27 @@
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+// We have to make the first letter of any such kind of program in JAVA must bu "Capital"
+        String a = ("Spoofy08 ");
+        String b = ("Plus");
+        int d = 5;
+        int c = 68;
+        System.out.println(a+b);
+        System.out.println(a + b);
+        System.out.println(c - d);
+        System.out.println(a+b+c+d);
+
+        double dividend = 100.0;
+        double divisor = 3.0;
+
+        // Perform division with floating-point precision
+        double quotient = dividend / divisor;
+
+        // Display the result
+        System.out.println("Result of division: " + quotient);
+       
+        
+        
+
+    }
+}
